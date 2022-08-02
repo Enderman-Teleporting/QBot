033283c07fcf4001b1e8fa73a17dc5bb1f8ad41b
王昱蘅 <sherrikevin5143@gmail.com>
2022-08-02 19:00:24 +0800

    avoided error

--- src/main/java/love/simbot/example/SimbotExampleApplication.java
+++ src/main/java/love/simbot/example/SimbotExampleApplication.java
@@ -9,21 +9,6 @@ import love.forte.simbot.bot.Bot;
 import love.forte.simbot.core.SimbotApp;
 import love.forte.simbot.core.SimbotContext;
 
-/**
- * simbot 启动类。
- * <p>
- * 此处的注解配置了两个配置文件：
- * <ul>
- *     <li>simbot.yml</li>
- *     <li>simbot-dev.yml</li>
- * </ul>
- * 其中，{@code simbot-dev.yml} 是一个测试环境的配置文件，只有当启动参数中存在 {@code --Sdev} 的时候才会被使用。
- * 如果你不需要一些特殊的配置文件，那么可以直接使用 {@code @SimbotApplication}.
- * <p>
- * 默认情况下，默认的配置文件名称为 {@code simbot.yml} 或 {@code simbot.properties}
- *
- * @author ForteScarlet
- */
 @SimbotApplication({
         @SimbotResource(value = "simbot.yml", orIgnore = true),
         @SimbotResource(value = "simbot-dev.yml", orIgnore = true, command = "dev"),
@@ -36,12 +21,5 @@ public class SimbotExampleApplication {
             第二个参数是main方法得到的启动参数。
          */
         SimbotApp.run(SimbotExampleApplication.class, args);
-    }
-    
-    @Override
-    public void post(@NotNull SimbotContext context) {
-    	Bot bot=context.getBotManager().getDefaultBot();
-    	BotSender sender =bot.getSender();
-    	sender.SENDER.sendPrivateMsg( code: /* */, msg:"已成功启动!");
     }
 }
