def4784bc50a4fe96741d7434706fca18ef21c7c
王昱蘅 <sherrikevin5143@gmail.com>
2022-08-02 18:53:54 +0800

    added function which are less important

--- src/main/java/love/simbot/example/listener/MyNewGroupMemberListen.java
+++ src/main/java/love/simbot/example/listener/MyNewGroupMemberListen.java
@@ -3,26 +3,16 @@ package love.simbot.example.listener;
 import love.forte.common.ioc.annotation.Beans;
 import love.forte.common.ioc.annotation.Depend;
 import love.forte.simbot.annotation.Listen;
-import love.forte.simbot.annotation.OnGroupAddRequest;
 import love.forte.simbot.annotation.OnGroupMemberIncrease;
 import love.forte.simbot.api.message.MessageContent;
 import love.forte.simbot.api.message.MessageContentBuilder;
 import love.forte.simbot.api.message.MessageContentBuilderFactory;
-import love.forte.simbot.api.message.Reply;
-import love.forte.simbot.api.message.assists.Flag;
 import love.forte.simbot.api.message.containers.AccountInfo;
-import love.forte.simbot.api.message.containers.BotInfo;
 import love.forte.simbot.api.message.containers.GroupInfo;
-import love.forte.simbot.api.message.events.GroupAddRequest;
 import love.forte.simbot.api.message.events.GroupMemberIncrease;
-import love.forte.simbot.api.message.events.ReduceEventGet;
+import love.forte.simbot.api.message.events.GroupMemberPermissionChanged;
+import love.forte.simbot.api.message.events.GroupNameChanged;
 import love.forte.simbot.api.sender.Sender;
-import love.forte.simbot.api.sender.Setter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 
 @Beans
@@ -42,10 +32,50 @@ public class MyNewGroupMemberListen {
         sender.sendGroupMsg(groupInfo, msg);
     }
 
-    @Listen(ReduceEventGet.class)
-    public void GroupMemberReduction(ReduceEventGet reduceEventGet, Sender sender){
-        AccountInfo accountinfored= reduceEventGet.getAccountInfo();
-        sender.sendGroupMsg(reduceEventGet.getGroupInfo, accountinfored+"离开了我们");
+    @Listen(GroupMemberPermissionChanged.class)
+    public void permissionChange(GroupMemberPermissionChanged groupMemberPermissionChanged, Sender sender){
+        AccountInfo accountInfoC=groupMemberPermissionChanged.getAccountInfo();
+        AccountInfo operatorinfo=groupMemberPermissionChanged.getOperatorInfo();
+        GroupInfo groupInfoC=groupMemberPermissionChanged.getGroupInfo();
+        MessageContentBuilder builder1 = messageBuilderFactory.getMessageContentBuilder();
+        boolean permissionlose=groupMemberPermissionChanged.isLostManagementRights();
+        boolean permissionget=groupMemberPermissionChanged.isGetManagementRights();
+        MessageContent message_;
+        if (permissionlose){
+            message_=builder1
+                .at(accountInfoC)
+                .text("的权限被")
+                .at(operatorinfo)
+                .text("失去管理权限")
+                .build();
+            sender.sendGroupMsg(groupInfoC, message_);
+        }
+        if (permissionget){
+            message_=builder1
+                .at(accountInfoC)
+                .text("的权限被")
+                .at(operatorinfo)
+                .text("获得管理权限")
+                .build();
+            sender.sendGroupMsg(groupInfoC, message_);
+        }
+        
+    }
+    @Listen(GroupNameChanged.class)
+    public void groupnamechange(GroupNameChanged groupNameChanged, Sender sender){
+        AccountInfo accountInfoGMC=groupNameChanged.getOperatorInfo();
+        GroupInfo groupInfoGMC=groupNameChanged.getGroupInfo();
+        String groupname=groupNameChanged.getAfterChange();
+        String groupnamebef=groupNameChanged.getBeforeChange();
+        MessageContentBuilder builder2=messageBuilderFactory.getMessageContentBuilder();
+        MessageContent message1=builder2
+            .at(accountInfoGMC)
+            .text("将群名称由")
+            .text(groupnamebef)
+            .text("改为")
+            .text(groupname)
+            .build();
+        sender.sendGroupMsg(groupInfoGMC,message1);
     }
 
 
--- src/main/java/love/simbot/example/listener/MyPrivateListen.java
+++ src/main/java/love/simbot/example/listener/MyPrivateListen.java
@@ -3,22 +3,23 @@ package love.simbot.example.listener;
 import catcode.CatCodeUtil;
 import love.forte.common.ioc.annotation.Beans;
 import love.forte.common.ioc.annotation.Depend;
+import love.forte.simbot.annotation.Listen;
 import love.forte.simbot.annotation.OnPrivate;
 import love.forte.simbot.api.message.MessageContent;
 import love.forte.simbot.api.message.MessageContentBuilder;
 import love.forte.simbot.api.message.MessageContentBuilderFactory;
+import love.forte.simbot.api.message.containers.AccountInfo;
+import love.forte.simbot.api.message.events.MsgGet;
 import love.forte.simbot.api.message.events.PrivateMsg;
-import love.forte.simbot.api.sender.MsgSender;
+import love.forte.simbot.api.message.events.PrivateMsgRecall;
 import love.forte.simbot.api.sender.Sender;
-import org.json.JSONObject;
 
-/**
- * 私聊消息监听的示例类。
- * 所有需要被管理的类都需要标注 {@link Beans} 注解。
- * @author ForteScarlet
- */
+import org.json.JSONObject;
 @Beans
 public class MyPrivateListen {
-    @OnPrivate
-    public void message(PrivateMsg msg, MsgSender sender);
+    @Listen(PrivateMsgRecall.class)
+    public void message(PrivateMsgRecall privateMsgRecall, Sender sender){
+        sender.sendPrivateMsg(privateMsgRecall, "快说,撤回了什么?!");
+    }
+    
 }
