3b95207d324760f66349f72d3352ec3c5932dbb8
王昱蘅 <sherrikevin5143@gmail.com>
2023-03-05 20:32:34 +0800

    dependencies

--- src/main/kotlin/com/bot/qbot/SimbotExampleApplication.kt
+++ src/main/kotlin/com/bot/qbot/SimbotExampleApplication.kt
@@ -6,10 +6,10 @@ import love.forte.simboot.core.SimbootApp
 import love.forte.simboot.core.SimbootApplication
 
 @SimbootApplication
-class Main
+class SimbotExampleApplication
 
 suspend fun main(args: Array<String>) {
         Log_settler.settle()
         properties_settler.settle()
-        SimbootApp.run(Main::class, *args).join()
+        SimbootApp.run(SimbotExampleApplication::class, *args).join()
 }
