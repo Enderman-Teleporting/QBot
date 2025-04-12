90e867eaa6d6abda02dff7ffa26be305d63c3698
王昱蘅 <sherrikevin5143@gmail.com>
2025-04-12 15:29:47 +0800

    ich möchte ein Minute/Monat/Jahr ...
      this guy is insane
    P.S. i am Chinese

--- src/main/java/io/github/et/Main.java
+++ src/main/java/io/github/et/Main.java
@@ -1,10 +1,12 @@
 package io.github.et;
 
+import com.alibaba.fastjson2.JSONArray;
 import io.github.et.eventListener.*;
 import io.github.et.exceptions.BotInfoNotFoundException;
 import io.github.et.games.roulette.Roulette;
 import io.github.et.messager.*;
 import io.github.et.tools.CommandConsole;
+import io.github.et.utils.JsonReader;
 import io.github.ettoolset.tools.logger.LevelNotMatchException;
 import io.github.ettoolset.tools.logger.Logger;
 import io.github.ettoolset.tools.logger.LoggerNotDeclaredException;
@@ -18,63 +20,100 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.Scanner;
+
+import static io.github.et.utils.JsonReader.jsonObject;
 
 public class Main {
     public static String URL = "v1/chat/completions";
     public static String Image_URL = "v1/images/generations";
     public static String APIKEY;
-    public static Properties botInfo;
     public static Bot bot;
+    private static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws IOException, BotInfoNotFoundException, RepeatedLoggerDeclarationException, LevelNotMatchException {
         Logger logger;
-        System.out.println("Trying to load bot info from ./botInfo.properties");
+        System.out.println("Trying to load bot info from ./botInfo.json");
         AnsiConsole.systemInstall();
-        try{
-            botInfo=new Properties();
-            botInfo.load(new BufferedReader(new FileReader("./botInfo.properties")));
-            System.out.println("Successfully loaded bot info");
-        }catch(IOException e) {
-            new File("./botInfo.properties").createNewFile();
-            throw new BotInfoNotFoundException("Cannot load bot info from file: ./botInfo.properties");
-        }
-        if (botInfo.getProperty("Log") == null) {
+        JsonReader.init();
+        if (jsonObject.get("log") == null) {
             logger=new Logger(Logger.Levels.DEBUG,null);
-        }
-        else if(botInfo.getProperty("Log").equals("null")){
+        } else if(jsonObject.get("log").equals("null")){
             logger=new Logger(Logger.Levels.DEBUG,null);
         }else{
-            logger=new Logger(Logger.Levels.DEBUG,botInfo.getProperty("Log"));
+            logger=new Logger(Logger.Levels.DEBUG, (String) jsonObject.get("log"));
+        }
+        if(jsonObject.get("port")==null|| !(jsonObject.get("port") instanceof Number)) {
+            System.out.println("请输入空闲端口");
+            String ans = sc.nextLine();
+            while (true) {
+                if (ans == null) {
+                    System.out.println("请重新输入");
+                    ans=sc.nextLine();
+                } else if (!ans.matches("^[0-9]+$")) {
+                    System.out.println("请输入数字");
+                    ans=sc.nextLine();
+                } else {
+                    if (Integer.parseInt(ans) > 65535 || Integer.parseInt(ans) < 0) {
+                        System.out.println("请输入0-65535之间的合法端口");
+                        ans=sc.nextLine();
+                    } else {
+                        jsonObject.put("port", Integer.parseInt(ans));
+                    }
+                }
+            }
+        }
+        if(jsonObject.get("id")==null||!(jsonObject.get("id") instanceof Number)){
+            System.out.println("请输入QQ号");
+            String ans=sc.nextLine();
+            while(true){
+                if(ans==null){
+                    System.out.println("请重新输入");
+                    ans=sc.nextLine();
+                }else if(!ans.matches("^[0-9]+$")){
+                    System.out.println("请输入数字");
+                    ans=sc.nextLine();
+                }else {
+                    jsonObject.put("id",Long.parseLong(ans));
+                }
+            }
         }
 
+        sc.close();
+        //TODO add napcat download code
         logger.debug("Initialized logger");
         //Deamon.runDeamon(RunMethod.CONSOLE);
-        bot= BotBuilder.positive(botInfo.getProperty("host"))
-                .token(botInfo.getProperty("token"))
+        bot= BotBuilder.positive((String) jsonObject.get("host"))
+                .token((String) jsonObject.get("token"))
                 .connect();
         if(bot==null){
             throw new BotInfoNotFoundException();
         }
         bot.login();
-        if(botInfo.get("DoReplyNudgeEvent").equals("true")){
+        if(jsonObject.get("doNudgeEvent") instanceof Boolean a){
+            if(a) {
+                bot.getEventChannel().registerListenerHost(new Nudger());
+                logger.fine("Registered listener Nudger");
+            }
+        }else if(jsonObject.get("doNudgeEvent") instanceof JSONArray a) {
             bot.getEventChannel().registerListenerHost(new Nudger());
             logger.fine("Registered listener Nudger");
+
         }
-        if(botInfo.get("Repeat").equals("true")){
+        if(jsonObject.get("repeat") instanceof Boolean a){
+            if(a) {
+                bot.getEventChannel().registerListenerHost(new Repeater());
+                logger.fine("Registered Listener Repeater");
+            }
+        }else if(jsonObject.get("repeat") instanceof JSONArray a) {
             bot.getEventChannel().registerListenerHost(new Repeater());
             logger.fine("Registered Listener Repeater");
         }
-        if(botInfo.get("Reply").equals("true")){
-            bot.getEventChannel().registerListenerHost(new Replier());
-            APIKEY= botInfo.getProperty("API_KEY","");
-            String urlPrefix= botInfo.getProperty("Url","");
-            if(urlPrefix.endsWith("/")){
-                URL=urlPrefix+URL;
-                Image_URL = urlPrefix + Image_URL;
-            }else{
-                URL=urlPrefix+"/"+URL;
-                Image_URL = urlPrefix + "/" + Image_URL;
+        if(jsonObject.get("reply")instanceof Boolean a){//TODO Null?JSONReader中直接加缺的搞成null或者[]
+            if (a) {
+                registerReplier();
             }
-            logger.fine("Registered listener Replier");
+        }else if(jsonObject.get("reply") instanceof JSONArray a) {
+            registerReplier();
         }
         if(botInfo.get("PassAddRequest").equals("true")){
             bot.getEventChannel().registerListenerHost(new RequestPasser());
@@ -127,6 +166,44 @@ public class Main {
         }).start();
         bot.join();
 
+    }
+
+
+    private static void registerReplier(){
+        Object b;
+        if ((b = jsonObject.get("API_KEY")) != null) {
+            if (b instanceof String s) {
+                APIKEY = s;
+            } else {
+                System.out.println("请输入你的API-KEY");
+                APIKEY = sc.nextLine();
+            }
+        } else {
+            System.out.println("请输入你的API-KEY");
+            APIKEY = sc.nextLine();
+        }
+        Object url;
+        String urlPrefix;
+        if ((url = jsonObject.get("url")) != null) {
+            if (url instanceof String s) {
+                urlPrefix = s;
+            } else {
+                System.out.println("请输入你的请求地址");
+                urlPrefix = sc.nextLine();
+            }
+        }else{
+            System.out.println("请输入你的请求地址");
+            urlPrefix = sc.nextLine();
+        }
+        if(urlPrefix.endsWith("/")){
+            URL=urlPrefix+URL;
+            Image_URL = urlPrefix + Image_URL;
+        }else{
+            URL=urlPrefix+"/"+URL;
+            Image_URL = urlPrefix + "/" + Image_URL;
+        }
+        bot.getEventChannel().registerListenerHost(new Replier());
+        logger.fine("Registered listener Replier");
     }
 
 }
