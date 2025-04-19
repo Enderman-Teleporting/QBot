7f3e76ba65ae01971aed3b2fca83101c7b06822e
王昱蘅 <sherrikevin5143@gmail.com>
2025-04-19 15:33:51 +0800

    apply lua 4 config

---
+++ addonConfigs/et/doReplyNudgeEvent.lua
@@ -0,0 +1,9 @@
+return{
+    feature = "DoReplyNudgeEvent",
+    guide={
+        "回复戳一戳"
+    },
+    config={
+        {name="doReplyNudgeEvent",type= { "java.lang.Boolean","com.alibaba.fastjson.JSONArray" },nullable=false},
+    }
+}
---
+++ addonConfigs/et/passAddRequest.lua
@@ -0,0 +1,9 @@
+return{
+    feature="PassAddRequest",
+    guide={
+        "自动通过加好友以及加群请求"
+    },
+    config={
+        {name="passAddRequest",type= { "java.lang.Boolean" },nullable=false}
+    }
+}
